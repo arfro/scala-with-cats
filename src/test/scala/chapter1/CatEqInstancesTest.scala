79f423b40ba3e516d391d202de24025d5abb141d
annette <aerfromenes@gmail.com>
2019-06-06 04:44:25 +0100

    added simple Cats Eq

--- src/test/scala/chapter1/CatEqInstancesTest.scala
+++ src/test/scala/chapter1/CatEqInstancesTest.scala
@@ -8,13 +8,12 @@ class CatEqInstancesTest extends UnitTest {
 
   val cat1 = Cat("cat", 2, "")
   val cat2 = Cat("cat", 22, "")
+  
+  // I'm not sure how to test syntax of triple equals from cats library, '===' by default refers to triple equals from scalaTest library
 
   "EqCats" should "allow to compare cat instances" in {
     import CatEqInstances._
     eqCat.eqv(cat1, cat2) shouldBe false
     cat1 =!= cat2 shouldBe true
   }
-
-  // I'm not sure how to test syntax of triple equals from cats library, '===' by default refers to triple equals from scalaTest library
-
 }
