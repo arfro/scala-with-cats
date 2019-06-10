23c68c324c9894676d45c86a15b2515db08d986e
annette <aerfromenes@gmail.com>
2019-06-10 20:11:59 +0100

    added SuperAdder monoid exercise + tests

---
+++ src/main/scala/chapter2/cats/SuperAdder.scala
@@ -0,0 +1,23 @@
+package chapter2.cats
+
+object SuperAdder {
+
+  // add for ints only
+  def add(items: List[Int]) = items.reduce(_ + _)
+
+  // add for Option[Int]
+  import cats.Monoid // for Monoid
+  import cats.syntax.semigroup._ // for |+|
+
+  def addMonoid[A](items: List[A])(implicit monoid: Monoid[A]): A = items.fold(monoid.empty)(_ |+| _)
+
+  // add for Orders!
+  case class Order(totalCost: Double, quantity: Double)
+
+  implicit val ordersMonoidCost: Monoid[Order] = new Monoid[Order] {
+    override def combine(x: Order, y: Order): Order = Order(x.totalCost + y.totalCost, x.quantity + y.quantity)
+    override def empty: Order = Order(0.0, 0.0)
+  }
+
+
+}
---
+++ src/test/scala/chapter2/cats/SuperAdderTest.scala
@@ -0,0 +1,29 @@
+package chapter2.cats
+
+import chapter2.cats.SuperAdder.Order
+import tests.UnitTest
+
+class SuperAdderTest extends UnitTest {
+
+  "SuperAdder - add" should "add integers" in {
+    SuperAdder.add(List(1, 2)) shouldBe 3
+  }
+
+  "SupperAdder - addOptions" should "add integers" in {
+    import cats.instances.int._
+    SuperAdder.addMonoid(List(1,2,3)) shouldBe 6
+  }
+
+  it should "add options of ints" in {
+    import cats.instances.option._
+    import cats.instances.int._
+    SuperAdder.addMonoid(List(Some(1), None, Some(3))) shouldBe Some(4)
+  }
+
+  "SupperAdder - adding orders" should "add two orders" in {
+    import SuperAdder.ordersMonoidCost
+    val order1 = Order(2, 4)
+    val order2 = Order(1, 3)
+    SuperAdder.addMonoid(List(order1, order2)) shouldBe Order(3, 7)
+  }
+}
